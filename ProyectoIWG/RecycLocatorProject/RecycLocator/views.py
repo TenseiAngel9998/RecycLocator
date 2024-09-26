b8edaa7a42ea21b670b71fcfbfe51da6fe6588be
Angel Berrios <164432745+TenseiAngel9998@users.noreply.github.com>
2024-09-26 20:50:02 -0300

    Delete ProyectoIWG/RecycLocatorProject directory

--- ProyectoIWG/RecycLocatorProject/RecycLocator/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    return render(request, 'RecycLocator/index.html')
-def mapa(request):
-    return render(request, 'RecycLocator/mapa.html')
