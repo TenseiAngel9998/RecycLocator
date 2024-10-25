f31b9626eb0e5edda9bfc4895c3031a34173018b
ItsIsmaaa <icastillod@usm.cl>
2024-10-25 00:56:53 -0300

    settings
    actualizado el archivo settings para hacerlo compatible con la base de datos

--- ProyectoIWG/RecycLocatorProject/RecycLocatorProject/settings.py
+++ ProyectoIWG/RecycLocatorProject/RecycLocatorProject/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'RecycLocatorProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': 'Recyclocator',                      
+        'USER': 'postgres',                         
+        'PASSWORD': 'Recyclocator',                
+        'HOST': 'localhost',                        
+        'PORT': '5432',     #->puerto predeterminado                       
     }
 }
+
 
 
 # Password validation
